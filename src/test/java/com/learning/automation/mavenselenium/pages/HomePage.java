aeed89552cdcdd03091ae8286493c4c5da4b03fa
techfundas <tech.naveenks@gmail.com>
2021-03-08 08:32:19 +0530

    page object model

--- src/test/java/com/learning/automation/mavenselenium/pages/HomePage.java
+++ src/test/java/com/learning/automation/mavenselenium/pages/HomePage.java
@@ -1,5 +1,22 @@
 package com.learning.automation.mavenselenium.pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+	//Constructor
+	//Elements
+	//Actions	
+		
+	//getDriver().findElement(By.xpath("/html/body/div[2]/div/div/button")).click();
+	
+	// Close button in the popup
+	@FindBy(xpath ="/html/body/div[2]/div/div/button")
+	private WebElement _btnClose;
+	
+	public void clickOnClose() {
+		_btnClose.click();
+	}
 
 }
